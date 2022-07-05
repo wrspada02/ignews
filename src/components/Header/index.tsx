de23557a39d5876202dad118d03671fc5d439c98
William Spada <williamrspada02@gmail.com>
2022-07-05 19:15:12 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { SignInButton } from '../SignInButton';
+import styles from './style.module.scss';
+
+export function Header(){
+    return(
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="ig.news logo" />
+                <nav>
+                    <a href="" className={styles.active}>Home</a>
+                    <a href="">Posts</a>
+                </nav>
+                <SignInButton/>
+            </div>
+        </header>
+    );
+}
