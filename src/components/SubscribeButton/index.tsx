7bc3ba8eb5e07450a4497305159c2221e47284f6
root <root@DESKTOP-HGD1NHP.localdomain>
2022-09-26 21:28:36 -0300

    feat: subscribe customer on stripe and save stripe_customer_id on faunadb

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,17 +1,18 @@
-import { signIn, useSession } from 'next-auth/react';
+import { signIn } from 'next-auth/react';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './style.module.scss';
+import { useSession } from 'next-auth/react';
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
-  const session = useSession();
+  const { status } = useSession();
   
   async function handleSubscribe() {
-    if(!session) {
+    if(status !== 'authenticated') {
       signIn('github');
       return;
     }
@@ -34,7 +35,7 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
     <button
       type='button'
       className={styles.subscribeButton}
-      onClick={() => handleSubscribe()}
+      onClick={handleSubscribe}
     >
       Subscribe Now
     </button>
